ced5f304c9d26b90915334b4227d02595116730a
johnpranoy7 <47387210+johnpranoy7@users.noreply.github.com>
2026-06-23 20:54:55 -0400

    Unary + ServerStreaming changes

--- src/main/java/com/johnp/grpcclient/GrpcClientApplication.java
+++ src/main/java/com/johnp/grpcclient/GrpcClientApplication.java
@@ -1,5 +1,6 @@
 package com.johnp.grpcclient;
 
+import com.johnp.grpc.CourseSummary;
 import com.johnp.grpc.StudentProfileResponse;
 import com.johnp.grpc.StudentsServiceGrpc;
 import com.johnp.grpcclient.service.StudentNetworkService;
@@ -9,8 +10,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.grpc.client.ImportGrpcClients;
 
+import java.util.List;
+
 @SpringBootApplication
-@ImportGrpcClients(target = "studentService", types = StudentsServiceGrpc.StudentsServiceBlockingStub.class)
+@ImportGrpcClients(target = "studentService",
+        types = {StudentsServiceGrpc.StudentsServiceBlockingStub.class, StudentsServiceGrpc.StudentsServiceStub.class})
 public class GrpcClientApplication implements CommandLineRunner {
 
     @Autowired
@@ -22,7 +26,7 @@ public class GrpcClientApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        StudentProfileResponse studentProfile = studentNetworkService.getStudentProfile(1);
-        System.out.println("Student Profile: " + studentProfile);
+//        studentNetworkService.getStudentProfile(1);
+        studentNetworkService.subscribeCourseCatalog();
     }
 }
--- src/main/java/com/johnp/grpcclient/service/StudentNetworkService.java
+++ src/main/java/com/johnp/grpcclient/service/StudentNetworkService.java
@@ -1,25 +1,56 @@
 package com.johnp.grpcclient.service;
 
+import com.google.protobuf.Empty;
+import com.johnp.grpc.CourseSummary;
 import com.johnp.grpc.StudentProfileRequest;
 import com.johnp.grpc.StudentProfileResponse;
 import com.johnp.grpc.StudentsServiceGrpc;
+import io.grpc.stub.StreamObserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StudentNetworkService {
 
+    //For Unary (REST Type Call)
     private final StudentsServiceGrpc.StudentsServiceBlockingStub studentsServiceBlockingStub;
 
+    //For Streaming (ClientStreaming, ServerStreaming, BidirectionalStreaming)
+    private final StudentsServiceGrpc.StudentsServiceStub studentsServiceStub;
+
     @Autowired
-    public StudentNetworkService(StudentsServiceGrpc.StudentsServiceBlockingStub studentsServiceBlockingStub) {
+    public StudentNetworkService(StudentsServiceGrpc.StudentsServiceBlockingStub studentsServiceBlockingStub, StudentsServiceGrpc.StudentsServiceStub studentsServiceStub) {
         this.studentsServiceBlockingStub = studentsServiceBlockingStub;
+        this.studentsServiceStub = studentsServiceStub;
     }
 
-    public StudentProfileResponse getStudentProfile(int studentId) {
+    public void getStudentProfile(int studentId) {
         StudentProfileRequest request = StudentProfileRequest.newBuilder()
                 .setStudentId(studentId)
                 .build();
-        return studentsServiceBlockingStub.getStudentProfile(request);
+        StudentProfileResponse studentProfile = studentsServiceBlockingStub.getStudentProfile(request);
+        System.out.println("Student Profile: " + studentProfile);
+    }
+
+    public void subscribeCourseCatalog() {
+        studentsServiceStub.streamCourseCatalog(Empty.newBuilder().build(), new StreamObserver<CourseSummary>() {
+            @Override
+            public void onNext(CourseSummary courseSummary) {
+                System.out.println("Course Summary: " + courseSummary);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.err.println("Error occurred while streaming course catalog: " + t.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Finished streaming course catalog.");
+            }
+        });
     }
 }
